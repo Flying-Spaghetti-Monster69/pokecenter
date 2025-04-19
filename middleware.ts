6fe060d34184b1dd02947b1813c9711caf026a2f
felipeGarzonMelguizo <felipegarzon908@gmail.com>
2025-04-19 16:16:07 -0500

    Feat: add authentication middleware to protect specific routes based on roles

---
+++ middleware.ts
@@ -0,0 +1,41 @@
+import { betterFetch } from "@better-fetch/fetch";
+import type { auth } from "@/utils/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+type Session = typeof auth.$Infer.Session;
+
+export async function middleware(request: NextRequest) {
+  const { data: session } = await betterFetch<Session>(
+    "/api/auth/get-session",
+    {
+      baseURL: request.nextUrl.origin,
+      headers: {
+        cookie: request.headers.get("cookie") || "", // Forward the cookies from the request
+      },
+    }
+  );
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  if (
+    session.user.role !== "user" &&
+    request.nextUrl.pathname.startsWith("/entrenador")
+  ) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  if (
+    session.user.role !== "admin" &&
+    request.nextUrl.pathname.startsWith("/enfermera")
+  ) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/entrenador/:path*", "/enfermera/:path*"], // Apply middleware to specific routes
+};
