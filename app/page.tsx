5f97ad1700c5480aff18679536ba167b1a919bd1
felipeGarzonMelguizo <felipegarzon908@gmail.com>
2025-04-19 23:33:52 -0500

    Feat: enhance home page with styled buttons for login and registration links

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { buttonStyles } from "@/utils/consts";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Link href="/auth/login">login</Link>
-      <Link href="/auth/registro">registro</Link>
+    <div className="flex flex-col items-center justify-center min-h-screen p-24">
+      <Link href="/auth/login">
+        <Button
+          className={`mb-4 bg-accent dark:bg-dark-accent ${buttonStyles}`}
+        >
+          Iniciar Sesión
+        </Button>
+      </Link>
+
+      <Link href="/auth/registro">
+        <Button
+          className={`mb-4 bg-accent dark:bg-dark-accent ${buttonStyles}`}
+        >
+          Registrarse
+        </Button>
+      </Link>
     </div>
   );
 }
