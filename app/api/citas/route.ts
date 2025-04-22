f393484ee21144fdeaf155df209f8c6c8c770590
felipeGarzonMelguizo <felipegarzon908@gmail.com>
2025-04-21 20:51:02 -0500

    Refactor: added POST logic to cita routes

--- app/api/citas/route.ts
+++ app/api/citas/route.ts
@@ -1,11 +1,30 @@
 import { NextResponse } from "next/server";
+import prisma from "@/utils/db";
+import { z } from "zod";
+import { citaFormSchema } from "@/utils/schemas";
+
+interface CitaForm extends z.infer<typeof citaFormSchema> {
+  userId: string;
+}
 
 export async function POST(request: Request) {
   try {
-    const requestBody = await request.json();
+    const requestBody: CitaForm = await request.json();
+
+    const citas = requestBody.pokemons.map((pokemon) => {
+      return { ...pokemon, userId: requestBody.userId };
+    });
+
+    const createAppointments = await prisma.cita.createMany({
+      data: citas,
+    });
 
     return NextResponse.json(
-      { message: "Data received successfully!", data: requestBody },
+      {
+        message: "Data received successfully!",
+        data: requestBody,
+        appointments: createAppointments,
+      },
       { status: 200 }
     );
   } catch (error) {
