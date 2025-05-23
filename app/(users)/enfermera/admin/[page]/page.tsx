0e67ed95e32faf9d572805569bf822b961da7984
felipeGarzonMelguizo <69770704+Flying-Spaghetti-Monster69@users.noreply.github.com>
2025-05-23 18:29:46 -0500

    Feat: added pagination and responsiveness to the admin page in enfermera pages

--- app/(users)/enfermera/admin/[page]/page.tsx
+++ app/(users)/enfermera/admin/[page]/page.tsx
@@ -1,3 +1,4 @@
+import AdminPagination from "@/components/admin/AdminPagination";
 import TableContents from "@/components/admin/TableContents";
 import LoadingBackdrop from "@/components/LoadingBackdrop";
 import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
@@ -11,7 +12,7 @@ const page = async ({ params }: params) => {
   const { page } = await params;
 
   return (
-    <main className="mt-16 px-16">
+    <main className="mt-16 lg:px-16">
       <h1 className="text-3xl mb-4 pt-8 font-semibold">Usuarios</h1>
       <Suspense
         fallback={
@@ -31,6 +32,9 @@ const page = async ({ params }: params) => {
           </TableHeader>
           <TableContents page={parseInt(page)} />
         </Table>
+        <footer>
+          <AdminPagination page={parseInt(page)} />
+        </footer>
       </Suspense>
     </main>
   );
---
+++ components/admin/AdminPagination.tsx
@@ -0,0 +1,103 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { getNumberOfPagesInAdmin } from "@/utils/actions";
+import { JSX } from "react";
+
+const baseUrl = "/enfermera/admin";
+
+const isInPages = (num: number, pages: number) => {
+  return num > 0 && num <= pages;
+};
+
+const getNumberPage = (number: number, isActive: boolean = false) => {
+  return (
+    <PaginationItem key={number}>
+      <PaginationLink href={`${baseUrl}/${number}`} isActive={isActive}>
+        {number}
+      </PaginationLink>
+    </PaginationItem>
+  );
+};
+
+const NearbyPages = (page: number, pages: number) => {
+  if (!isInPages(page, pages)) {
+    return (
+      <PaginationItem>
+        <PaginationLink href={`${baseUrl}/1`}>1</PaginationLink>
+      </PaginationItem>
+    );
+  }
+  const pagination: JSX.Element[] = [];
+
+  if (page - 2 > 0) {
+    pagination.push(
+      <PaginationItem key={page - 2}>
+        <PaginationEllipsis />
+      </PaginationItem>
+    );
+  }
+
+  if (page - 1 > 0) {
+    pagination.push(getNumberPage(page - 1));
+  }
+
+  pagination.push(getNumberPage(page, true));
+
+  let nextPages = page;
+
+  while (nextPages < pages) {
+    nextPages++;
+
+    if (nextPages === page + 3) {
+      pagination.push(
+        <PaginationItem key={page + 3}>
+          <PaginationEllipsis />
+        </PaginationItem>
+      );
+      break;
+    }
+
+    pagination.push(getNumberPage(nextPages));
+  }
+
+  return pagination;
+};
+
+const AdminPagination = async ({ page }: { page: number }) => {
+  const pages = await getNumberOfPagesInAdmin();
+
+  if (pages === 0) {
+    return <h1>No tenemos usuarios! :C</h1>;
+  }
+
+  if (!pages) {
+    return <h1>hubo un error</h1>;
+  }
+
+  return (
+    <Pagination className="mt-2">
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious
+            href={page - 1 > 0 ? `/enfermera/admin/${page - 1}` : "#"}
+          />
+        </PaginationItem>
+        {NearbyPages(page, pages)}
+        <PaginationItem>
+          <PaginationNext
+            href={page + 1 <= pages ? `/enfermera/admin/${page + 1}` : "#"}
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+};
+
+export default AdminPagination;
